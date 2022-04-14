7f576c0863e2295800639f002345844700dba2ab
HASSAN1A <okothhassanjuma@gmail.com>
2022-04-15 00:51:31 +0300

    Returns the string representation of the float

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Returns the string representation of the float """
+
+
+def to_str(n: float) -> str:
+    """ takes a float n and returns the string of the float """
+    return (str(n))
