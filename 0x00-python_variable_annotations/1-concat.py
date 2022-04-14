bbf56d0cbb422c13dec6d000c295a1a64ee5e6cf
HASSAN1A <okothhassanjuma@gmail.com>
2022-04-15 00:50:27 +0300

    Returns a concatenated string

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" Returns a concatenated string """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ functions that takes two strings, returns a concatenated string """
+    return (str1 + str2)
