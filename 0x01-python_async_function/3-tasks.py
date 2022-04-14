6f44df8cd9ebd300e73f54feba432271e3b1ed5d
HASSAN1A <okothhassanjuma@gmail.com>
2022-04-15 01:02:37 +0300

    Asynch function basics

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Asynch function basics'''
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """returns an asyncio Task"""
+    return asyncio.create_task(wait_random(max_delay))
